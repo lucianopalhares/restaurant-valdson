a9e563b9dfea7f0c147d3715c68c3030aac5e4f6
Luciano Palhares <lucianopalharesrosa@hotmail.com>
2020-01-29 13:12:50 -0300

    routes.php to original

--- routes/web.php
+++ routes/web.php
@@ -11,27 +11,6 @@
 |
 */
 
-Route::redirect('lara-admin','login');
-Route::get('/','HomeController@index')->name('welcome');
-Route::post('/reservation','ReservationController@reserve')->name('reservation.reserve');
-Route::post('/contact','ContactController@sendMessage')->name('contact.send');
-
-Auth::routes();
-
-Route::group(['prefix'=>'admin','middleware'=>'auth','namespace'=>'Admin'], function (){
-    Route::get('dashboard', 'DashboardController@index')->name('admin.dashboard');
-    Route::resource('slider','SliderController');
-    Route::resource('category','CategoryController');
-    Route::resource('item','ItemController');
-    Route::get('reservation','ReservationController@index')->name('reservation.index');
-    Route::post('reservation/{id}','ReservationController@status')->name('reservation.status');
-    Route::delete('reservation/{id}','ReservationController@destory')->name('reservation.destory');
-
-    Route::get('contact','ContactController@index')->name('contact.index');
-    Route::get('contact/{id}','ContactController@show')->name('contact.show');
-    Route::delete('contact/{id}','ContactController@destroy')->name('contact.destroy');
-
-    Route::resource('restaurant', 'RestaurantController');
-    Route::resource('paymentWay', 'PaymentWayController');
-    Route::resource('role', 'RoleController');
+Route::get('/', function () {
+    return view('welcome');
 });
