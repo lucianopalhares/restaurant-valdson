115fca2a100462839ae866db5415e15b61842a74
Luciano Palhares <lucianopalharesrosa@hotmail.com>
2020-01-29 14:16:14 -0300

    edit route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,18 +6,10 @@ use App\Category;
 use App\Item;
 use App\Slider;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //$this->middleware('auth');
-    }
 
     /**
      * Show the application dashboard.
