55b287582af7d8565debbdc3beeecd01de9528dd
Luciano Palhares <lucianopalharesrosa@hotmail.com>
2020-02-08 09:19:10 -0300

    route/web

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,39 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/errors',function(){
+  return view('error');
+});
 
-Route::get('/','HomeController@index')->name('welcome');
+Route::get('/',function(){
+  return redirect('/login');
+});
+
+Auth::routes();
+
+Route::group(['prefix'=>'admin','middleware'=>'auth','namespace'=>'Admin','as'=>'Admin'], function (){
+
+    Route::get('dashboard','DashboardController@index', array("as"=>"dashboard","name"=>"dashboard"));
+    Route::resource('cargo','RoleController', array("as"=>"cargo","name"=>"cargo"));
+    Route::resource('restaurante','RestaurantController', array("as"=>"restaurante","name"=>"restaurante"));  
+    Route::resource('formaPagamento','PaymentWayController', array("as"=>"formaPagamento","name"=>"formaPagamento"));
+    Route::resource('categoria','CategoryController', array("as"=>"categoria","name"=>"categoria"));
+});
+Route::group(['prefix'=>'usuario','middleware'=>'auth','namespace'=>'User','as'=>'User'], function (){
+         
+    Route::group([
+        'prefix' => '{restaurantSlug}',
+        'middleware' => 'restaurant'
+    ], function () {
+        Route::get('dashboard', [
+            'as'   => 'dashboard',
+            'uses' => 'DashboardController@index',
+        ]);
+         
+        Route::resource('cardapio','ItemController', array("as"=>"cardapio","name"=>"cardapio"));
+        Route::resource('restaurante_categoria','RestaurantCategoryController', array("as"=>"restaurante_categoria","name"=>"restaurante_categoria"));
+  });
+});
+      
+
+    
